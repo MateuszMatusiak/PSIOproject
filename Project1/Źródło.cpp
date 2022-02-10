fb21b8f9ba2b379c9fd65531f848ee0cf2372109
MateuszMatusiak <94836196+MateuszMatusiak@users.noreply.github.com>
2022-02-10 02:12:56 +0100

    Update Źródło.cpp

--- Project1/Źródło.cpp
+++ Project1/Źródło.cpp
@@ -5,143 +5,228 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
+#define PATH "2.mp4"
+
 using namespace cv;
 using namespace std;
 
-Mat imgOriginal;
-Mat imgGray;
-Mat imgBlur;
-Mat imgCanny;
-Mat imgThre;
-Mat imgDilate;
-Mat imgErode;
-Mat imgWarp;
-Mat imgCrop;
+class MaxArea {
+public:
+	Mat mat;
+
+	bool isContour;
+	int idx;
+	double area;
+
+	double peri;
+
+	vector<Point> contour;
+	vector<Point> conPoly;
+	vector<Point> wrong;
+	Rect rect;
+
+	MaxArea() {
+		this->isContour = false;
+		this->idx = 0;
+		this->area = 0;
+	}
+
+	MaxArea(vector<vector<Point>> contours, Mat mat) {
+		this->mat = mat;
+		this->isContour = false;
+		this->idx = 0;
+		this->area = 0;
+		Set(contours);
+	}
+
+	MaxArea(const MaxArea& maxArea) {
+		mat = maxArea.mat;
+		isContour = maxArea.isContour;
+		idx = maxArea.idx;
+		area = maxArea.area;
+		contour = maxArea.contour;
+		conPoly = maxArea.conPoly;
+		rect = maxArea.rect;
+	}
+
+	void Reset() {
+		isContour = false;
+		idx = 0;
+		area = 0;
+
+		peri = 0;
+
+		contour = vector<Point>();
+		conPoly = vector<Point>();;
+		wrong = vector<Point>();;
+		rect = Rect();
+	}
+
+	void Set(vector<vector<Point>> contours) {
+		int size = contours.size();
+
+		if (size <= 0)
+			return;
+
+		idx = 0;
+		for (int i = 0; i < size; i++)
+		{
+			double tempArea = contourArea(contours[i]);
+			if (tempArea > area)
+			{
+				area = tempArea;
+				idx = i;
+			}
+		}
+
+		this->contour = contours[idx];
+	}
+
+	void UpdateMax(int size) {
+		this->conPoly = vector<Point>(size);
+		this->rect = Rect();
+		this->isContour = true;
+		peri = arcLength(contour, true);
+	}
+
+	vector<Point> getConPolyVector() {
+		if (!isContour)
+			return vector<Point>();
+
+		approxPolyDP(contour, conPoly, 0.02 * peri, true);
+
+		wrong = conPoly;
+		conPoly = Optimize(conPoly);
+
+		return conPoly;
+	}
+
+
+	struct line {
+		Point A;
+		Point B;
+		bool out;
+
+		line(Point A, Point B) {
+			this->A = A;
+			this->B = B;
+			out = false;
+		}
+
+		double length()
+		{
+			return sqrt(pow((B.x - A.x), 2) + pow((B.y - A.y), 2));
+		}
+	};
+
+	bool ccw(Point A, Point  B, Point  C) {
+		return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x);
+	}
+
+	bool intersect(line l1, line l2) {
+		Point A = l1.A;
+		Point B = l1.B;
+		Point C = l2.A;
+		Point D = l2.B;
+		return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D);
+	}
+
+	vector<Point> findMax(Mat x) {
+		vector<vector<Point>> contours;
+		vector<Vec4i> hierarchy;
+
+		findContours(x, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+		vector<vector<Point>> conPoly(contours.size());
+		vector<Rect> boundRect(contours.size());
+
+		vector<Point> biggest;
+		double maxArea = 0;
+
+		for (int i = 0; i < contours.size(); i++)
+		{
+			double area = contourArea(contours[i]);
+			if (area > 1000)
+			{
+				double peri = arcLength(contours[i], true);
+				approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+
+				if (area > maxArea && conPoly[i].size() == 4) {
+					biggest = { conPoly[i][0],conPoly[i][1] ,conPoly[i][2] ,conPoly[i][3] };
+					maxArea = area;
+				}
+			}
+		}
+		return biggest;
+	}
+
+	vector<Point> Optimize(vector<Point> p) {
+		vector<line> l;
+		for (int i = 0; i < p.size() - 1; ++i)
+			for (int j = i + 1; j < p.size(); ++j) {
+				l.push_back(line(p[i], p[j]));
+			}
+
+		Mat res(mat.size(), CV_8UC3, cv::Scalar(0, 0, 0));
+
+		for (int i = 0; i < l.size(); ++i)
+			for (int j = i + 1; j < l.size(); ++j) {
+				cv::line(res, l[i].A, l[i].B, Scalar(0, 255, 255), 1);
+				cv::line(res, l[j].A, l[j].B, Scalar(0, 255, 255), 1);
+
+				if (intersect(l[i], l[j]))
+				{
+					if (l[i].A != l[j].A
+						&& l[i].A != l[j].B
+						&& l[i].B != l[j].B
+						&& l[i].B != l[j].A) {
+						l[i].out = true;
+						l[j].out = true;
+					}
+				}
+			}
+
+		cvtColor(res, res, COLOR_BGR2GRAY);
+
+		return findMax(res);
+	}
+};
+
+int currentVideo = 0;
+int prevVideo = 0;
 
 VideoCapture cap;
-vector<Point> MaxContour;
-bool isContour = false;
-vector<vector<Point>> MaxConPoly;
-vector<Rect> MaxBoundRect;
 
 Mat Result;
 int clipIndex = 0;
 
-//int hmin = 24, hmax = 0, smin = 113, smax = 130, vmin = 243, vmax = 217;
-// 3 int hmin = 49, hmax = 0, smin = 55, smax = 99, vmin = 255, vmax = 99;
- int hmin = 31, hmax = 6, smin = 119, smax = 102, vmin = 242, vmax = 199;
+Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+MaxArea maxArea = MaxArea();
 
-vector<vector<int>> HSVmask{	{hmin, smin, vmin, hmax, smax, vmax},
-								{hmin, smin, vmin, hmax, smax, vmax}, 
-								{hmin, smin, vmin, hmax, smax, vmax}, 
-								{hmin, smin, vmin, hmax, smax, vmax}, 
-								{hmin, smin, vmin, hmax, smax, vmax}, 
-								{hmin, smin, vmin, hmax, smax, vmax}, };
+int b = 1;
+int sigma = 6;
+int areaLimit = 10000;
 
-void preProcessing(Mat x) {
+int reflectionMin = 50;
+int reflectionMax = 180;
 
-	cvtColor(x, imgGray, COLOR_BGR2GRAY);
-	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
-	Canny(imgBlur, imgCanny, 25, 75);
-	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
-	dilate(imgCanny, imgDilate, kernel);
-	erode(imgDilate, imgErode, kernel);
+int hmin = 19, hmax = 6, smin = 121, smax = 99, vmin = 213, vmax = 238;
+
+Mat preProcessing(Mat source) {
+	Mat gray;
+	Mat blur;
+	Mat canny;
+	Mat Dilate;
+	Mat Erode;
+
+	cvtColor(source, gray, COLOR_BGR2GRAY);
+	GaussianBlur(gray, blur, Size(3, 3), 3, 0);
+	Canny(blur, canny, 25, 75);
+	dilate(canny, Dilate, kernel);
+	erode(Dilate, Erode, kernel);
+	return Erode;
 }
 
-vector<Point> getContours(Mat x) {
-
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
-
-	findContours(x, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	//drawContours(x, contours, -1, Scalar(255, 0, 255), 2);
-	vector<vector<Point>> conPoly(contours.size());
-	vector<Rect> boundRect(contours.size());
-
-	vector<Point> biggest;
-	double maxArea = 0;
-
-	int maxIndex = -1;
-	
-	for (int i = 0; i < contours.size(); i++)
-	{
-		double area = contourArea(contours[i]);
-		if (area > maxArea)
-		{
-			maxArea = area;
-			maxIndex = i;
-		}
-	}
-
-	int i = maxIndex;
-	if (maxArea > 1000)
-	{
-		cout << maxArea << endl;
-		MaxContour = contours[i];
-		isContour = true;
-		MaxConPoly = vector<vector<Point>>(contours.size());
-		MaxBoundRect = vector<Rect>(contours.size());
-	}
-
-	if (isContour) {
-		double peri = arcLength(MaxContour, true);
-		approxPolyDP(MaxContour, MaxConPoly[i], 0.02 * peri, true);
-
-		if (MaxConPoly[i].size() == 4) {
-
-			//drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 5);
-			drawContours(imgOriginal, MaxConPoly, i, Scalar(255, 0, 255), 2);
-			biggest = { MaxConPoly[i][0],MaxConPoly[i][1] ,MaxConPoly[i][2] ,MaxConPoly[i][3] };
-			rectangle(imgOriginal, MaxBoundRect[i].tl(), MaxBoundRect[i].br(), Scalar(0, 255, 0), 5);
-		}
-	}
-
-	return biggest;
-}
-
-bool isOKcolor(Mat x) {
-
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
-
-	findContours(x, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	//drawContours(x, contours, -1, Scalar(255, 0, 255), 2);
-	vector<vector<Point>> conPoly(contours.size());
-	vector<Rect> boundRect(contours.size());
-
-
-	for (int i = 0; i < contours.size(); i++)
-	{
-		double area = contourArea(contours[i]);
-		cout << area<< endl;
-		if (area > 370000&&area<500000)
-		{
-				return true;
-		}
-	}
-	return false;
-}
-
-vector<Point> reorder(vector<Point> points)
-{
-	vector<Point> newPoints;
-	vector<int>  sumPoints, subPoints;
-
-	for (int i = 0; i < 4; i++)
-	{
-		sumPoints.push_back(points[i].x + points[i].y);
-		subPoints.push_back(points[i].x - points[i].y);
-	}
-
-	newPoints.push_back(points[min_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); //0
-	newPoints.push_back(points[max_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //1
-	newPoints.push_back(points[min_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //2
-	newPoints.push_back(points[max_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); //3
-
-	return newPoints;
-}
-
-void getWarp(vector<Point> points, float width, float height)
+Mat getWarp(Mat source, vector<Point> points, float width, float height)
 {
 	Point2f src[4] = { points[0],
 					   points[1],
@@ -154,44 +239,25 @@ void getWarp(vector<Point> points, float width, float height)
 					   {width,	height} };
 
 	Mat matrix = getPerspectiveTransform(src, dst);
-	warpPerspective(imgOriginal, imgWarp, matrix, Point(width, height));
-	return;
+	Mat imgWarp;
+	warpPerspective(source, imgWarp, matrix, Point(width, height));
+	return imgWarp;
 }
 
 void HSVtrackbars() {
 
 	namedWindow("Trackbars", (640, 200));
-	createTrackbar("Hue Min", "Trackbars", &hmin, 179);
-	createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+	createTrackbar("Hue Min", "Trackbars", &hmin, 255);
+	createTrackbar("Hue Max", "Trackbars", &hmax, 255);
 	createTrackbar("Sat Min", "Trackbars", &smin, 255);
 	createTrackbar("Sat Max", "Trackbars", &smax, 255);
 	createTrackbar("Val Min", "Trackbars", &vmin, 255);
 	createTrackbar("Val Max", "Trackbars", &vmax, 255);
+	createTrackbar("ST.....", "Trackbars", &b, 1);
+	createTrackbar("Sigma..", "Trackbars", &sigma, 6);
+	createTrackbar("ReflectionMin", "Trackbars", &reflectionMin, 255);
+	createTrackbar("ReflectionMax", "Trackbars", &reflectionMax, 255);
 }
-
-
-Mat checkColor(Mat img)
-{
-	Mat HSV;
-	cvtColor(img, HSV, COLOR_BGR2HSV);
-	/*Scalar lower(HSVmask[0], HSVmask[1], HSVmask[2]);
-	Scalar upper(HSVmask[3], HSVmask[4], HSVmask[5]);*/
-
-	Scalar lower(hmin, hmax, smin);
-	Scalar upper(smax, vmin, vmax);
-	Mat mask;
-	inRange(HSV, lower, upper, mask);
-	//imshow("mask", mask);
-	//vector<Point> initialPoints = getContours(mask);
-	/*if (isOKcolor(mask))
-	{
-		imshow("res", img);
-	}*/
-
-	return mask;
-}
-
-
 
 Mat getClip(int index = -1) {
 	Mat toReturn;
@@ -203,8 +269,10 @@ Mat getClip(int index = -1) {
 			return toReturn;
 		}
 	}
-	cap.read(Result);
-	resize(Result, Result, Size(), 0.5, 0.5);
+	if (b) {
+		cap.read(Result);
+		resize(Result, Result, Size(), 0.5, 0.5);
+	}
 	Result.copyTo(toReturn);
 	return toReturn;
 }
@@ -214,144 +282,155 @@ void LoadVideo(string name) {
 	cap = VideoCapture(recPath + name);
 }
 
+Mat getMask(Mat img)
+{
+	Mat HSV;
+	cvtColor(img, HSV, COLOR_BGR2HSV);
+
+	Scalar lower(hmin, hmax, smin);
+	Scalar upper(smax, vmin, vmax);
+	Mat mask;
+	inRange(HSV, lower, upper, mask);
+	return mask;
+}
+
+vector<Point> getContours(Mat source) {
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	Mat temp;
+	Result.copyTo(temp);
+
+	findContours(source, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+	MaxArea area(contours, source);
+
+	if (area.area > areaLimit) {
+		maxArea = area;
+		maxArea.UpdateMax(contours.size());
+	}
+
+	return maxArea.getConPolyVector();
+}
+
+void RemoveReflections(Mat source) {
+	Mat gray;
+	cvtColor(source, gray, COLOR_RGB2GRAY);
+
+	for (int j = 0; j < gray.rows; j++)
+		for (int i = 0; i < gray.cols; i++)
+		{
+			int x = gray.at<uchar>(j, i);
+			if (x > reflectionMax) {
+				gray.at<uchar>(j, i) = reflectionMax;
+				source.at<Vec3b>(j, i)[0] = reflectionMax;
+				source.at<Vec3b>(j, i)[1] = reflectionMax;
+				source.at<Vec3b>(j, i)[2] = reflectionMax;
+			}
+			if (x < reflectionMin) {
+				gray.at<uchar>(j, i) = reflectionMin;
+				source.at<Vec3b>(j, i)[0] = reflectionMin;
+				source.at<Vec3b>(j, i)[1] = reflectionMin;
+				source.at<Vec3b>(j, i)[2] = reflectionMin;
+			}
+		}
+}
+
+Mat Marge(Mat mask, Mat Erode) {
+	Mat result;
+	Erode.copyTo(result);
+	for (int j = 0; j < mask.rows; j++)
+		for (int i = 0; i < mask.cols; i++)
+			if (mask.at<uchar>(j, i) < 128)
+				result.at<uchar>(j, i) = 0;
+	return result;
+}
+
+bool isOKcolor(Mat x) {
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(x, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+		double area = contourArea(contours[i]);
+		if (area > 370000 && area < 500000)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 int main() {
 
 	vector<Point> initialPoints, docPoints;
-	
-	LoadVideo("4.mp4");
-	
-	HSVtrackbars();
-	int i = 0;
+	Mat Original;
+
+	namedWindow("Trackbars", 0x00000100);
+	createTrackbar("Video", "Trackbars", &currentVideo, 2);
+
+	//HSVtrackbars();
+
+	LoadVideo(PATH);
+
 	while (true) {
-		++i;
-		imgOriginal = getClip();
 
-		waitKey(1);
+		if (currentVideo != prevVideo) {
+			destroyAllWindows();
+			namedWindow("Trackbars", 0x00000100);
+			createTrackbar("Video", "Trackbars", &currentVideo, 2);
+			LoadVideo(to_string(currentVideo + 1) + ".mp4");
+			prevVideo = currentVideo;
+		}
 
+		Original = getClip();
+		waitKey(30);
 
-		/*
-			/*ret, thresh2 = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
-			ret, thresh3 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
-			ret, thresh4 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
-			ret, thresh5 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)*/
+		Mat withoutReflections;
+		Original.copyTo(withoutReflections);
 
-			
+		RemoveReflections(withoutReflections);
 
-		//Nie tykaæ
-		//-------------------------------------
-		//imshow("Image", imgOriginal);
-		////waitKey(10);
-		//checkColor(imgOriginal);
-		//continue;
-		//-------------------------------------
+		Mat Erode = preProcessing(withoutReflections);
 
-		int height = imgOriginal.size[0];
-		int width = imgOriginal.size[1];
+		Mat mask = getMask(Original);
+		Mat dilateMask;
+		mask.copyTo(dilateMask);
 
-		/*Mat hsv;
-		cvtColor(imgOriginal, hsv, COLOR_BGR2HSV);
+		Mat kernel2 = getStructuringElement(MORPH_RECT, Size(sigma + 1, sigma + 1));
+		dilate(mask, dilateMask, kernel2);
 
-		for (int j = 0; j < imgOriginal.rows; j++)
-			for (int i = 0; i < imgOriginal.cols; i++)
-				hsv.at<Vec3b>(j, i)[1] = 255;
+		Mat marged = Marge(dilateMask, Erode);
 
-		cvtColor(hsv, imgOriginal, COLOR_HSV2BGR);*/
+		initialPoints = getContours(marged);
+		imshow("Image", Original);
 
-		//dilate(imgOriginal, imgOriginal, Mat::ones(3, 3, 0)); //Mat.ones(new Size(3, 3), 0)
-
-		Mat grayC;
-		cvtColor(imgOriginal, grayC, COLOR_RGB2GRAY);
-		Mat imgToProc;
-		imgOriginal.copyTo(imgToProc);
-		//threshold(grayC, imgToProc, 180, 180, THRESH_TRUNC);
-
-
-		for (int j = 0; j < grayC.rows; j++)
-			for (int i = 0; i < grayC.cols; i++)
-			{
-				int x = grayC.at<uchar>(j, i);
-				if (x > 180) {
-					grayC.at<uchar>(j, i) = 50;
-					imgToProc.at<Vec3b>(j, i)[0] = 50;
-					imgToProc.at<Vec3b>(j, i)[1] = 50;
-					imgToProc.at<Vec3b>(j, i)[2] = 50;
-				}
-				if (x < 50) {
-					grayC.at<uchar>(j, i) = 50;
-					imgToProc.at<Vec3b>(j, i)[0] = 50;
-					imgToProc.at<Vec3b>(j, i)[1] = 50;
-					imgToProc.at<Vec3b>(j, i)[2] = 50;
-				}
-			}
-
-		//imshow("proc", imgToProc);
-		
-		preProcessing(imgOriginal);
-		imshow("Erode1", imgErode);
-		
-		
-		Mat mask = checkColor(imgOriginal);
-		Mat maskCopy;
-		mask.copyTo(maskCopy);
-
-		int sigma = 2;
-		for (int j = 0; j < mask.rows; j++)
-			for (int i = 0; i < mask.cols; i++)
-			{
-				if (mask.at<bool>(j, i) != 0)
-				{
-					for (int k = -sigma; k < sigma; ++k)
-						for (int l = -sigma; l < sigma; ++l)
-						{
-							int y = j + k;
-							int x = i + l;
-							if (x < 0 || y < 0 || x > mask.cols - 1 || y > mask.rows - 1)
-								continue;
-							maskCopy.at<uchar>(y, x) = 255;
-						}
-				}
-			}
-
-		//Mat kernel2 = getStructuringElement(MORPH_RECT, Size(8, 8));
-		//dilate(mask, mask, kernel2);
-
-
-
-
-		imshow("Mask", maskCopy);
-
-		for (int j = 0; j < grayC.rows; j++)
-			for (int i = 0; i < grayC.cols; i++)
-			{
-				if (maskCopy.at<bool>(j, i) == 0)
-					imgErode.at<bool>(j, i) = 0;
-			}
-
-		Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
-		dilate(imgErode, imgErode, kernel);
-
-		imshow("Erode2", imgErode);
-
-		initialPoints = getContours(imgErode);
-		if (initialPoints.size() < 4) {
-			imshow("Image", imgOriginal);
+		if (initialPoints.size() != 4) {
 			continue;
 		}
-		docPoints = reorder(initialPoints);
-		getWarp(docPoints, (float)width, (float)height);
+
+		convexHull(initialPoints, docPoints);
+		auto p = docPoints;
+		docPoints = { p[0],p[1],p[3],p[2] };
+
+		Mat imgWarp = getWarp(Original, docPoints, (float)Original.cols, (float)Original.rows);
 
 		int cropVal = 5;
-		Rect roi(cropVal, cropVal, width - (2 * cropVal), height - (2 * cropVal));
-		imgCrop = imgWarp(roi);
+		Rect roi(cropVal, cropVal, Original.cols - (2 * cropVal), Original.rows - (2 * cropVal));
 
-		//checkColor(imgCrop);
+		Mat imgCrop = imgWarp(roi);
 
-		imshow("Image", imgOriginal);
-		//imshow("Image Dilation", imgThre);
-		//imshow("Image Warp", img.Warp);
-		
-
-		imshow("Image Crop", imgCrop);
+		if (isOKcolor(getMask(imgCrop))) {
+			imshow("Result", imgCrop);
+			maxArea.Reset();
+		}
 	}
 	return 0;
 }
